ac8b3ef27565ff3434fd8fdef9116aa528d72461
HazukiMiki <xzhou92@gmail.com>
2022-11-29 17:58:13 -0500

    think it's finish

---
+++ app.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector("[data-hamburger]")
+
+const navLinks = document.querySelector(".icon-li")
+const bg = document.querySelector(".bg-thing")
+
+hamburger.addEventListener("pointerup", navMenu)
+bg.addEventListener("pointerup", navMenu)
+function navMenu() {
+    if (navLinks.style.display === "grid") {
+        hamburger.setAttribute("Src", "images/icon-hamburger.svg")
+        navLinks.style.display = "none"
+        bg.classList.remove("bg")
+
+    } else {
+        hamburger.setAttribute("Src", "images/icon-close.svg")
+        navLinks.style.display = "grid"
+        bg.classList.add("bg")
+        navLinks.classList.add("slide-bottom")
+    }
+}
